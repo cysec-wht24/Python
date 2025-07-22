6baa824a5d074fe36bdc4257fa30952ff1acea7c
Man-Of-May <bmanomay2018@gmail.com>
2025-07-22 13:49:48 +0000

    trying

---
+++ 09_Generators/01_basics.py
@@ -0,0 +1,30 @@
+# https://docs.python.org/3.15/glossary.html#term-generator
+
+# Use generators when want to save memory
+# don't want the results immediately
+# lazy evaluation
+
+def serve_chai():
+    yield "Cup 1: Masala Chai"
+    yield "Cup 2: Ginger Chai"
+    yield "Cup 3: Elaichi Chai"
+
+stall = serve_chai()
+
+# for cup in stall:
+#     print(cup)
+
+def get_chai_list():
+    return ["Cup 1", "Cup 2", "Cup 3"]
+
+# generator function
+def get_chai_gen():
+    yield "Cup 1"
+    yield "Cup 2"
+    yield "Cup 3"
+
+chai = get_chai_gen()
+print(next(chai))
+print(next(chai))
+print(next(chai))
+# print(next(chai)) # gives error
