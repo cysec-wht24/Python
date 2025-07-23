32f350acbfffdf136e1785ba767775c8a10ec233
Man-Of-May <bmanomay2018@gmail.com>
2025-07-23 21:20:11 +0000

    Setting up pydantic

---
+++ 14_Pydantic/01_intro.py
@@ -0,0 +1,3 @@
+# Pydantic is a python library that provides certain things that are helpful in web dev as well as ML
+# Data Validation, Setting Mangement, Data parsing, API development, Data Serilization/deserilaization
+print("Start of pydantic journey")
